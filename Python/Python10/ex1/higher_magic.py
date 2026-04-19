d9fc9f272c771aef5ba733db7de1b53bb260a687
mednine9 <ninemed52@gmail.com>
2026-04-19 20:53:08 +0100

    3la lah

--- Python/Python10/ex1/higher_magic.py
+++ Python/Python10/ex1/higher_magic.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     mega_fireball = power_amplifier(fireball, 3)
     original = fireball("Dragon", 10)
     amplified = mega_fireball("Dragon", 10)
-    print(f"Original: 10, Amplified: 30")
+    print("Original: 10, Amplified: 30")
     print(f"  -> {original}")
     print(f"  -> {amplified}")
 
